a472a3f0baaa3d965045f0fe985708b374ab158b
Esteban Morell <estebanmorellb@gmail.com>
2019-10-30 22:40:54 -0500

    Updates

---
+++ precache-manifest.6900da7bd9294dec752083f15e37f1ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b57fef7880e5c5151bee5f5eef784a9",
+    "url": "/Astros-Clicky-Game/index.html"
+  },
+  {
+    "revision": "fcb474209c15199c4963",
+    "url": "/Astros-Clicky-Game/static/css/main.2b42138e.chunk.css"
+  },
+  {
+    "revision": "ec7e7baeb333024a19cc",
+    "url": "/Astros-Clicky-Game/static/js/2.7b82c88d.chunk.js"
+  },
+  {
+    "revision": "fcb474209c15199c4963",
+    "url": "/Astros-Clicky-Game/static/js/main.8ffe58b8.chunk.js"
+  },
+  {
+    "revision": "184d4d5210a749289356",
+    "url": "/Astros-Clicky-Game/static/js/runtime-main.4a4f02ff.js"
+  },
+  {
+    "revision": "dc97f37e85277ba890077c337104ea64",
+    "url": "/Astros-Clicky-Game/static/media/background.dc97f37e.jpg"
+  }
+]);
